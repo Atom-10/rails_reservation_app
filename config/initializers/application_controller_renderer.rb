5d462a5ddf7bce476180a25a31f5710ed50a5b21
biot <gdwsura@yahoo.co.jp>
2023-11-23 16:17:31 +0900

    ユーザー編集ページを修正

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ddedb0f4214ad99621d28ab169c534cfe8a227d9e9f25cb6b6a4507f9975b3ea2d96065ce16dd1800bfdd68f6607c1dca5a0f8051183ab7fc4817938e2dc57df'
