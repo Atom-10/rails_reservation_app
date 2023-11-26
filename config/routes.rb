e09be345914f5760f239968aec0c1ba052940fd4
biot <gdwsura@yahoo.co.jp>
2023-11-26 18:48:41 +0900

    アカウント編集ページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root to: "home#index"
   resources :users
   resources :rooms
+  get "/account" => "users#account"
+  get "user/account" => "devise/passwords#edit"
 end
