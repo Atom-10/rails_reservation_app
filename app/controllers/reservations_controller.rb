d5542a068f2f824ec9aa42fade0458d9477f1cd4
biot <gdwsura@yahoo.co.jp>
2023-12-02 23:52:27 +0900

    アクションを設定

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,13 +2,26 @@ class ReservationsController < ApplicationController
   def index
   end
 
-  def show
-  end
-
   def new
     @reservation = Reservation.new
   end
 
+  def show
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+    @reservation.user_id = current_user.id
+    @reservation.save
+    redirect_to reservation_path(@reservation)
+  end
+
   def edit
+  end
+
+  private
+  def reservation_params
+    params.require(:reservation).permit(:check_in, :check_out, :guest)
   end
 end
