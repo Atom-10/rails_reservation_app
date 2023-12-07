709a4a27255c0f6d6d407de2195b19241e497197
biot <gdwsura@yahoo.co.jp>
2023-12-07 15:46:03 +0900

    ルーティングを変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
   resources :users
-  resources :rooms
-  resources :reservations
+  resources :rooms do
+    resources :reservations, only: [:create]
+  end
+  resources :reservations do
+    member do
+      get "confirmation"
+    end
+  end
   get "/account" => "users#account"
   get "user/account" => "devise/passwords#edit"
   get "/user_room" => "rooms#user_room"
