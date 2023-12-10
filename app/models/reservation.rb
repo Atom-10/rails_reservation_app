7cc64c0fd41e6a54e21e7472cb90230bd13cdbc4
biot <gdwsura@yahoo.co.jp>
2023-12-10 23:27:00 +0900

    ビューを修正

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,7 +1,4 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :room
-  validates :check_in, presence: true
-  validates :check_out, presence: true
-  validates :guest, numericality: { greater_than_or_equal_to: 1 }
 end
